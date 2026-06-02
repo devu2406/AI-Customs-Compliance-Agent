8c0d2288702bc2aeb54d04e9f1c14ae2a9350036
devu2406 <dr0566@srmist.edu.in>
2026-06-02 12:14:38 +0530

    Added document preview panel

--- script.js
+++ script.js
@@ -14,3 +14,35 @@ function uploadFiles(){
     <p>OCR Processing Started...</p>
     `;
 }
+const dropZone =
+document.querySelector(".drop-zone");
+
+dropZone.addEventListener("dragover",e=>{
+    e.preventDefault();
+});
+
+dropZone.addEventListener("drop",e=>{
+    e.preventDefault();
+
+    const files =
+    e.dataTransfer.files;
+
+    showFiles(files);
+});
+function showFiles(files){
+
+    const fileList =
+    document.getElementById("fileList");
+
+    fileList.innerHTML="";
+
+    for(let file of files){
+
+        let li=
+        document.createElement("li");
+
+        li.textContent=file.name;
+
+        fileList.appendChild(li);
+    }
+}
